3ab1e98a1c54ec67feb8f88a567dc7496cbc8ab5
githubmhjao <ntumhjao@gmail.com>
2020-01-02 20:21:54 +0800

    Update SGAN.py

--- SGAN.py
+++ SGAN.py
@@ -26,9 +26,9 @@ Latent_Dim = 10
 Class_Num = 10
 
 
-# ------------------ #
-#  Basic Parameters  #
-# ------------------ #
+# ----------- #
+#  Load Data  #
+# ----------- #
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
 
